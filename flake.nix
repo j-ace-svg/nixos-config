aecc9547aa1674bfa3e331e966a1f0f1c5c9f301
John Montgomery <j.ace.svg@gmail.com>
2024-05-24 10:10:09 -0500

    67 current  2024-05-24 10:03:13  24.05.20240306.9df3e30   6.6.19                          *

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.j-ace-svg = import ./home.nix;
+            home-manager.users.j-ace-svg = import ./j-ace-svg/home.nix;
           }
         ];
       };
