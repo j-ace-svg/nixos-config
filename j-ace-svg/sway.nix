9addf87229b53bc0cc0206f886984641429a4d6f
John Montgomery <j.ace.svg@gmail.com>
2024-05-24 10:20:49 -0500

    67 current  2024-05-24 10:03:13  24.05.20240306.9df3e30   6.6.19                          *

---
+++ j-ace-svg/sway.nix
@@ -0,0 +1,69 @@
+{pkgs, ...}: {
+  # Window manager
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      input = {
+        "*" = {
+          xkb_layout = "us,us";
+          xkb_variant = "dvorak,";
+          xkb_options = "ctrl:nocaps,ctrl:lctrl_meta,shift:both_capslock,grp:rctrl_toggle";
+        };
+      };
+      output = {
+        "*" = {
+          bg = "/run/current-system/sw/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png fill";
+        };
+      };
+      bars = [
+        {
+          mode = "dock";
+          hiddenState = "hide";
+          position = "top";
+          workspaceButtons = true;
+          workspaceNumbers = true;
+          statusCommand = "${pkgs.i3status}/bin/i3status";
+          fonts = {
+            names = ["monospace"];
+            size = 8.0;
+          };
+          trayOutput = "primary";
+          colors = {
+            background = "#000000";
+            statusline = "#ffffff";
+            separator = "#666666";
+            focusedWorkspace = {
+              border = "#4c7899";
+              background = "#285577";
+              text = "#ffffff";
+            };
+            activeWorkspace = {
+              border = "#333333";
+              background = "#5f676a";
+              text = "#ffffff";
+            };
+            inactiveWorkspace = {
+              border = "#333333";
+              background = "#222222";
+              text = "#888888";
+            };
+            urgentWorkspace = {
+              border = "#2f343a";
+              background = "#900000";
+              text = "#ffffff";
+            };
+            bindingMode = {
+              border = "#2f343a";
+              background = "#900000";
+              text = "#ffffff";
+            };
+          };
+        }
+      ];
+    };
+    extraConfig = ''
+      exec snap run accountable2you
+    '';
+  };
+}
