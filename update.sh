4ffe62e34d9f51471bc0f46cfa9057f34637e8a7
John Montgomery <j.ace.svg@gmail.com>
2024-06-04 22:18:33 -0500

    81 current  2024-06-04 22:18:28  24.11.20240531.57610d2   6.6.32                          *

--- update.sh
+++ update.sh
@@ -5,21 +5,24 @@ set -e
 
 echo "NixOS Updating..."
 
+# Store previous generation metadata
+previous=$(nixos-rebuild list-generations | grep current)
+
 # Rebuild, output simplified errors, log trackebacks
 sudo sh -c 'nix flake update &> /etc/nixos/nixos-update.log' || (cat /etc/nixos/nixos-update.log | grep --color error && exit 1)
 sudo sh -c 'nixos-rebuild --flake /etc/nixos#nixos switch &> /etc/nixos/nixos-switch.log' || (cat /etc/nixos/nixos-switch.log | grep --color error && exit 1)
 
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
 # Early return if no changes were detected
-if sudo git diff --quiet; then
+if [[ "$previous" == "$current" ]]; then
     echo "No changes detected, exiting."
     exit 0
 fi
 
 # Shows your changes
 sudo git diff -U0
-
-# Get current generation metadata
-current=$(nixos-rebuild list-generations | grep current)
 
 # Commit all changes witih the generation metadata
 sudo git commit -am "$current"
