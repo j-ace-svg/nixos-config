4ac92ba183eb99fcccb8b71a03d6801207487fcd
John Montgomery <j.ace.svg@gmail.com>
2024-06-21 02:06:50 -0500

    93 current  2024-06-21 02:06:47  24.11.20240605.e8057b6  6.6.32                          *

--- j-ace-svg/rnnoise/default.nix
+++ j-ace-svg/rnnoise/default.nix
@@ -1,4 +1,9 @@
 {pkgs, ...}: {
+  xdg.configFile."pipewire/pipewire.conf.d/99-input-denoising.conf" = {
+    enable = true;
+    text = builtins.readFile ./99-input-denoising.conf;
+  };
+
   home.packages = [
     pkgs.rnnoise
   ];
