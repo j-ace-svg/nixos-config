cbead2f42c38acef45a4f8f2b25f10f4eaeb205a
John Montgomery <j.ace.svg@gmail.com>
2024-07-26 22:32:57 -0500

    117 current  2024-07-26 22:32:52  24.11.20240714.693bc46  6.6.39                          *

--- j-ace-svg/mpv/default.nix
+++ j-ace-svg/mpv/default.nix
@@ -1,6 +1,23 @@
 {pkgs, ...}: {
   programs.mpv = {
     enable = true;
+    scripts = [
+      pkgs.mpvScripts.sponsorblock
+      pkgs.mpvScripts.quality-menu
+      pkgs.mpvScripts.thumbfast
+    ];
+    bindings = {
+      "<" = "add chapter -1";
+      ">" = "add chapter 1";
+      "PGDWN" = "playlist-prev";
+      "PGUP" = "playlist-next";
+      "WHEEL_DOWN" = "ignore";
+      "WHEEL_UP" = "ignore";
+      "MBTN_LEFT" = "cycle pause";
+      "F" = "script-binding quality_menu/video_formats_toggle";
+      "Alt+f" = "script-binding quality_menu/audio_formats_toggle";
+      "F12" = "ignore";
+    };
   };
 
   home.packages = [
