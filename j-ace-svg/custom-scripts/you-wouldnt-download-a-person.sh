9fcc8f4291aaf42234c755bf84b8d2f00d19b435
John Montgomery <j.ace.svg@gmail.com>
2024-12-19 21:00:11 -0600

    174 current  2024-12-19 21:00:04  24.11.20241111.dc460ec  6.6.60                          *

--- j-ace-svg/custom-scripts/you-wouldnt-download-a-person.sh
+++ j-ace-svg/custom-scripts/you-wouldnt-download-a-person.sh
@@ -3,6 +3,7 @@
 # Make script fail if any individual commands fail
 set -e
 
+echo "$#"
 while [[ $# != 1 ]]; do
     yt-dlp -xo "%(channel)s/%(playlist_title)s/%(playlist_index)s %(title)s.%(ext)s" "$1"; wait
     shift
