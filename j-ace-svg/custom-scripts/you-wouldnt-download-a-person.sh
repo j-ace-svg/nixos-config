af81c34f488f9b2d7943831a7a330d5243b419b4
John Montgomery <j.ace.svg@gmail.com>
2024-12-20 12:08:02 -0600

    176 current  2024-12-20 12:07:54  24.11.20241111.dc460ec  6.6.60                          *

--- j-ace-svg/custom-scripts/you-wouldnt-download-a-person.sh
+++ j-ace-svg/custom-scripts/you-wouldnt-download-a-person.sh
@@ -4,6 +4,11 @@
 set -e
 
 while [[ $# != 0 ]]; do
+    pushd
+    channel="$(yt-dlp --print channel "$1")"
+    mkdir "$channel"
+    cd channel
     yt-dlp -xo "%(channel)s/%(playlist_title)s/%(playlist_index)s %(title)s.%(ext)s" "$1"; wait
     shift
+    popd
 done
