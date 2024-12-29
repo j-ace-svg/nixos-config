801292a5d4e90117f01d70b6f178be07261b3572
John Montgomery <j.ace.svg@gmail.com>
2024-12-28 21:54:42 -0600

    30 current  2024-12-28 21:54:38  25.05.20241219.d70bd19   6.6.66                          *

--- home-manager/j-ace-svg/firefox/default.nix
+++ home-manager/j-ace-svg/firefox/default.nix
@@ -7,8 +7,52 @@
     enable = true;
     package = pkgs.firefox-beta;
     policies = {
-      DisablePocket = true;
+      DontCheckDefaultBrowser = true;
       DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      #DisableFirefoxScreenshots = true;
+
+      #DisplayBookmarksToolbar = "never";
+      DisplayMenuBar = "never"; # Previously appeared when pressing alt
+
+      OverrideFirstRunPage = "";
+      PictureInPicture.Enabled = false;
+      PromptForDownloadLocation = false;
+
+      HardwareAcceleration = true;
+      TranslateEnabled = true;
+
+      Homepage.StartPage = "previous-session";
+
+      UserMessaging = {
+        UrlbarInterventions = false;
+        SkipOnboarding = true;
+      };
+
+      FirefoxSuggest = {
+        WebSuggestions = false;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+      };
+
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      FirefoxHome =
+        # Make new tab only show search
+        {
+          Search = true;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          SponsoredPocket = false;
+          Snippets = false;
+        };
     };
 
     profiles.j-ace-svg = {
