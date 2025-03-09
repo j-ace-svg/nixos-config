815c4635cfa9a912c12138f5a31c4a1b6a4226ea
John Montgomery <j.ace.svg@gmail.com>
2025-03-09 13:05:37 -0500

    delphiki: 124 current  2025-03-08 16:02:54  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,13 +1,14 @@
 {
   pkgs,
   inputs,
+  opts,
   ...
 }: {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     users.j-ace-svg.imports = [./j-ace-svg/home.nix];
-    extraSpecialArgs = {inherit inputs;};
+    extraSpecialArgs = {inherit inputs opts;};
     backupFileExtension = "hm-backup";
   };
 }
