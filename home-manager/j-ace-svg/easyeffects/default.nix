3e17c81ba8ddac7079331caa8c348ea5175bb0b3
John Montgomery <j.ace.svg@gmail.com>
2025-03-15 18:49:55 -0500

    wiggin: 229 current  2025-03-15 17:36:34  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/j-ace-svg/easyeffects/default.nix
+++ home-manager/j-ace-svg/easyeffects/default.nix
@@ -4,19 +4,4 @@
   ...
 }: {
   services.easyeffects.enable = true;
-
-  #systemd.user.services.easyeffects = {
-  #  Unit = {
-  #    Description = "Audio effects for PipeWire applications";
-  #  };
-  #  Install = {
-  #    WantedBy = ["default.target"];
-  #  };
-  #  Service = {
-  #    ExecStart = lib.escapeShellArgs [
-  #      "${pkgs.easyeffects}/bin/easyeffects"
-  #      "--gapplication-service"
-  #    ];
-  #  };
-  #};
 }
