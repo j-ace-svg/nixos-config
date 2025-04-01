2f03bc725a925a343be8e6cc9525c1193e483cc8
John Montgomery <j.ace.svg@gmail.com>
2025-03-31 22:25:47 -0500

    delphiki: 165 current  2025-03-31 22:25:22  25.05.20250218.73cf49b   6.12.14                          *

---
+++ home-manager/j-ace-svg/nvim/after/plugin/keymaps/init.lua
@@ -0,0 +1,11 @@
+local Remap = require("j-ace-svg.keymap")
+local nnrm = Remap.nnoremap
+local inrm = Remap.inoremap
+local onrm = Remap.onoremap
+local cnrm = Remap.cnoremap
+local vnrm = Remap.vnoremap
+local tnrm = Remap.tnoremap
+local nmap = Remap.nmap
+local ia = Remap.ia
+local ca = Remap.ca
+
---
+++ home-manager/j-ace-svg/nvim/lua/plugins/ultisnips.lua
@@ -0,0 +1,11 @@
+return {
+  "SirVer/ultisnips",
+  event = "InsertEnter",
+  ft = "snippets",
+  init = function()
+    vim.g.UltiSnipsSnippetDirectories = "UltiSnips"
+    vim.g.UltiSnipsExpandTrigger = "<tab>"
+    vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+    vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
+  end
+}
