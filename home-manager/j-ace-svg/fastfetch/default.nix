637b5b954530d8b4d4bd5221cffedb1c77e7e108
John Montgomery <j.ace.svg@gmail.com>
2025-04-08 18:43:45 -0500

    delphiki: 186 current  2025-04-08 18:43:20  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/j-ace-svg/fastfetch/default.nix
+++ home-manager/j-ace-svg/fastfetch/default.nix
@@ -9,6 +9,28 @@
       logo = {
         source = ./aperature-nix.txt;
       };
+      display = {
+        size = {
+          binaryPrefix = "si";
+        };
+        color = "blue";
+        separator = "  ";
+      };
+      modules = [
+        {
+          type = "datetime";
+          key = "Date";
+          format = "{1}-{3}-{11}";
+        }
+        {
+          type = "datetime";
+          key = "Time";
+          format = "{14}:{17}:{20}";
+        }
+        "break"
+        "player"
+        "media"
+      ];
     };
   };
 }
