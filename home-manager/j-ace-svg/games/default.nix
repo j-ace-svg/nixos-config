85f77302b2606a87e5bf9f9cbe70c1f81522aa6d
John Montgomery <j.ace.svg@gmail.com>
2025-04-20 23:25:02 -0500

    wiggin: 279 current  2025-04-20 23:24:47  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/j-ace-svg/games/default.nix
+++ home-manager/j-ace-svg/games/default.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   home.file.".local/share/Steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg/autoexec.cfg" = {
-    source = ./csgo-autoexec.cfg;
+    source = ./csgo/autoexec.cfg;
     executable = true;
   };
 
