2a76d858e9445050a2e6dd382f984263b913bb4e
John Montgomery <j.ace.svg@gmail.com>
2025-04-21 20:48:17 -0500

    delphiki: 214 current  2025-04-21 20:48:08  25.05.20250218.73cf49b   6.12.14                          *

---
+++ home-manager/j-ace-svg/nvim/after/plugin/firenvim.lua
@@ -0,0 +1,15 @@
+local Remap = require("j-ace-svg.keymap")
+local nnrm = Remap.nnoremap
+local inrm = Remap.inoremap
+local onrm = Remap.onoremap
+local cnrm = Remap.cnoremap
+local vnrm = Remap.vnoremap
+local tnrm = Remap.tnoremap
+local nmap = Remap.nmap
+local ia = Remap.ia
+local ca = Remap.ca
+
+if vim.g.started_by_firenvim == true then
+  vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Cmd>call firenvim#focus_page()<CR>", {})
+  vim.opt.signcolumn = "no"
+end
