33a62dd14addc84e31ac8c914a6f55808d8acf1f
John Montgomery <j.ace.svg@gmail.com>
2025-04-21 21:20:59 -0500

    delphiki: 221 current  2025-04-21 21:20:51  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/j-ace-svg/zathura/default.nix
+++ home-manager/j-ace-svg/zathura/default.nix
@@ -10,7 +10,7 @@
       recolor = true;
       recolor-lightcolor = "#282828";
       recolor-darkcolor = "#ebdbb2";
-      default-bg = "rgba(40, 40, 40, 178)"; #B3
+      default-bg = "rgba(15.625%, 15.625%, 15.625%, 0.7)"; #B3
       default-fg = "#ebdbb2";
     };
   };
