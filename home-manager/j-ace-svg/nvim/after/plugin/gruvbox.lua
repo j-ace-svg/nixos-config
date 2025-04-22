41d3c0dd84bda3572f30cadf675291f8a194b36f
John Montgomery <j.ace.svg@gmail.com>
2025-04-21 23:25:41 -0500

    delphiki: 222 current  2025-04-21 23:25:32  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/j-ace-svg/nvim/after/plugin/gruvbox.lua
+++ home-manager/j-ace-svg/nvim/after/plugin/gruvbox.lua
@@ -1,6 +1,8 @@
 vim.cmd.colorscheme("gruvbox")
 
-vim.api.nvim_set_hl(0, 'Normal', {guibg=nil})
-vim.api.nvim_set_hl(0, 'NonText', {guibg=nil})
-vim.api.nvim_set_hl(0, 'Normal', {ctermbg=nil})
-vim.api.nvim_set_hl(0, 'NonText', {ctermbg=nil})
+if vim.g.started_by_firenvim == false then
+  vim.api.nvim_set_hl(0, 'Normal', {guibg=nil})
+  vim.api.nvim_set_hl(0, 'NonText', {guibg=nil})
+  vim.api.nvim_set_hl(0, 'Normal', {ctermbg=nil})
+  vim.api.nvim_set_hl(0, 'NonText', {ctermbg=nil})
+end
