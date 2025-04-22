efb216a678683cf1d6351c18238dfcca26fdef5e
John Montgomery <j.ace.svg@gmail.com>
2025-04-22 08:18:00 -0500

    delphiki: 224 current  2025-04-22 08:17:22  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/j-ace-svg/nvim/after/plugin/firenvim.lua
+++ home-manager/j-ace-svg/nvim/after/plugin/firenvim.lua
@@ -11,6 +11,6 @@ local ca = Remap.ca
 
 if vim.g.started_by_firenvim == true then
   vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Cmd>call firenvim#focus_page()<CR>", {})
-  vim.opt.signcolumn = "no"
+  vim.opt_local.signcolumn = "no"
   vim.opt.colorcolumn = ""
 end
--- home-manager/j-ace-svg/nvim/after/plugin/gruvbox.lua
+++ home-manager/j-ace-svg/nvim/after/plugin/gruvbox.lua
@@ -1,6 +1,6 @@
 vim.cmd.colorscheme("gruvbox")
 
-if vim.g.started_by_firenvim == false then
+if vim.g.started_by_firenvim == nil then
   vim.api.nvim_set_hl(0, 'Normal', {guibg=nil})
   vim.api.nvim_set_hl(0, 'NonText', {guibg=nil})
   vim.api.nvim_set_hl(0, 'Normal', {ctermbg=nil})
