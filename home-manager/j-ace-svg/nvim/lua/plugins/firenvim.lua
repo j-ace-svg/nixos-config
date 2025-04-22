c038d745f096c4010394ab1b9e8f7dec70ff65ca
John Montgomery <j.ace.svg@gmail.com>
2025-04-21 20:24:07 -0500

    delphiki: 212 current  2025-04-21 20:23:59  25.05.20250218.73cf49b   6.12.14                          *

--- home-manager/j-ace-svg/nvim/lua/plugins/firenvim.lua
+++ home-manager/j-ace-svg/nvim/lua/plugins/firenvim.lua
@@ -2,5 +2,11 @@ return {
   "glacambre/firenvim",
   build = ":call firenvim#install(0)",
   init = function()
+    vim.g.firenvim_config = {
+      globalSettings = {
+        ['<C-w>'] = 'noop',
+        ['<C-n>'] = 'default'
+      }
+    }
   end
 }
