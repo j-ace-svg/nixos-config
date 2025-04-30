ce7baa4eb00ef72bc1a4b4052bf87bbba522cae8
John Montgomery <j.ace.svg@gmail.com>
2025-04-29 21:46:25 -0500

    wiggin: 288 current  2025-04-29 21:46:10  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/j-ace-svg/legcord/default.nix
+++ home-manager/j-ace-svg/legcord/default.nix
@@ -7,7 +7,7 @@
 }: {
   home.file = {
     ".config/legcord/storage/settings.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${opts.configPath}/settings.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${opts.configPath}/home-manager/j-ace-svg/legcord/settings.json";
     };
   };
 
