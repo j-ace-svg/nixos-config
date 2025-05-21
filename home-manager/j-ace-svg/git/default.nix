3a63b044e147daee29b2a8da608e8a2a0a905a28
John Montgomery <j.ace.svg@gmail.com>
2025-05-21 16:39:35 -0500

    wiggin: 294 current  2025-05-21 16:39:19  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/j-ace-svg/git/default.nix
+++ home-manager/j-ace-svg/git/default.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
     lfs.enable = true;
     userEmail = "j.ace.svg@gmail.com";
     userName = "J.ace.svg";
+    extraConfig = {
+      core = {
+        sshCommand = "ssh -F ${config.home.homeDirectory}/.ssh/config";
+      };
+    };
   };
 
   home.packages = [
