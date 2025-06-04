bd4159bc2d7c51db6dd5692b81a0d1730470e94d
John Montgomery <j.ace.svg@gmail.com>
2025-06-03 23:00:45 -0500

    wiggin: 326 current  2025-06-03 20:50:41  25.05.20250218.73cf49b  6.12.14                          *

--- nixos/hosts/wiggin/sops.nix
+++ nixos/hosts/wiggin/sops.nix
@@ -8,8 +8,6 @@
   sops = {
     age = {
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-      keyFile = "/var/lib/sops-nix/key.txt";
-      generateKey = true;
     };
     defaultSopsFile = ./secrets.yaml;
   };
