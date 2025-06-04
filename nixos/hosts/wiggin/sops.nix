7206fe0617b08567f088aa92bc280ab25a47d7f5
John Montgomery <j.ace.svg@gmail.com>
2025-06-04 15:08:08 -0500

    wiggin: 326 current  2025-06-03 20:50:41  25.05.20250218.73cf49b  6.12.14                          *

--- nixos/hosts/wiggin/sops.nix
+++ nixos/hosts/wiggin/sops.nix
@@ -4,10 +4,14 @@
   pkgs,
   modulesPath,
   ...
-}: {
+}: let
+  isEd25519 = k: k.type == "ed25519";
+  getKeyPath = k: k.path;
+  keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
+in {
   sops = {
     age = {
-      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      sshKeyPaths = map getKeyPath keys;
     };
     defaultSopsFile = ./secrets.yaml;
   };
