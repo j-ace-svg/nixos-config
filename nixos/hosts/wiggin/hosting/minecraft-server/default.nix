08109f62fe4eb6de74743aa4d4ccdab2c854c16c
John Montgomery <j.ace.svg@gmail.com>
2025-07-05 01:58:34 -0500

    wiggin: 440 current  2025-07-05 01:58:05  25.11.20250603.c2a0396  6.12.31                          *

--- nixos/hosts/wiggin/hosting/minecraft-server/default.nix
+++ nixos/hosts/wiggin/hosting/minecraft-server/default.nix
@@ -11,10 +11,42 @@
   ];
   nixpkgs.overlays = [inputs.nix-minecraft.overlay];
 
-  sops = {
-    secrets = {
-    };
+  sops = let
+    whitelist-count = 4;
+    inc-num-string = x: builtins.toString (x + 1);
+  in {
+    secrets =
+      builtins.listToAttrs (
+        builtins.genList (
+          num: {
+            name = "minecraft-server/whitelist/user-${inc-num-string num}/uuid";
+            value = {sopsFile = ./secrets.yaml;};
+          }
+        )
+        whitelist-count
+        ++ builtins.genList (
+          num: {
+            name = "minecraft-server/whitelist/user-${inc-num-string num}/name";
+            value = {sopsFile = ./secrets.yaml;};
+          }
+        )
+        whitelist-count
+      )
+      // {
+      };
     templates = {
+      "minecraft-server/whitelist.json" = {
+        content = builtins.toJSON (
+          builtins.genList (
+            num: {
+              "uuid" = config.sops.placeholder."minecraft-server/whitelist/user-${inc-num-string num}/uuid";
+              "name" = config.sops.placeholder."minecraft-server/whitelist/user-${inc-num-string num}/name";
+            }
+          )
+          whitelist-count
+        );
+        owner = config.services.minecraft-servers.user;
+      };
     };
   };
 
@@ -31,6 +63,10 @@
         serverProperties = {
           server-port = 25565;
           motd = "Self-hosted by yours truly!";
+        };
+
+        files = {
+          "whitelist.json" = config.sops.templates."minecraft-server/whitelist.json".path;
         };
       };
       # Name for next server (follow order of planets visited by Andrew Wiggin)
