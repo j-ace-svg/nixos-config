937a070a3d2477af938b7e867f1b5a3002c61a5d
John Montgomery <j.ace.svg@gmail.com>
2025-07-05 20:03:57 -0500

    wiggin: 442 current  2025-07-05 20:03:29  25.11.20250603.c2a0396  6.12.31                          *

--- home-manager/j-ace-svg/sway/default.nix
+++ home-manager/j-ace-svg/sway/default.nix
@@ -363,6 +363,48 @@ in {
     };
   };
 
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "loginctl lock-session";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+      {
+        label = "logout";
+        action = "loginctl terminate-user $USER";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "u";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+    ];
+  };
+
   xdg = {
     userDirs = {
       enable = true;
