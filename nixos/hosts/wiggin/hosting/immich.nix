e170e093ce8c564683e8941afc6262bb8fa36dac
John Montgomery <j.ace.svg@gmail.com>
2025-07-07 12:16:01 -0500

    wiggin: 454 current  2025-07-07 12:15:40  25.11.20250603.c2a0396  6.12.31                          *

--- nixos/hosts/wiggin/hosting/immich.nix
+++ nixos/hosts/wiggin/hosting/immich.nix
@@ -19,4 +19,26 @@
     openFirewall = true;
     host = "immich.${config.local.hosting.domain}";
   };
+
+  services.nginx.virtualHosts.${config.services.immich.host} = {
+    #forceSSL = true;
+    #useACMEHost = "acmechallenge.${config.local.hosting.domain}";
+    #acmeRoot = null;
+    locations = {
+      "/" = {
+        proxyPass = "http://[::1]:${toString config.services.immich.port}";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
+          client_max_body_size 50000M;
+          proxy_read_timeout   600s;
+          proxy_send_timeout   600s;
+          send_timeout         600s;
+        '';
+      };
+      #inherit (config.services.nginx.virtualHosts."acmechallenge.${config.local.hosting.domain}".locations) "/.well-known/acme-challenge";
+    };
+  };
+
+  #security.acme.certs."acmechallenge.${config.local.hosting.domain}".extraDomainNames = [config.services.immich.host];
 }
