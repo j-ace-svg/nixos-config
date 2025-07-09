2b6ab8c1c0bd271d036ccf508520a28c6f9b36fa
John Montgomery <j.ace.svg@gmail.com>
2025-07-09 14:25:40 -0500

    wiggin: 463 current  2025-07-09 14:25:10  25.11.20250603.c2a0396  6.12.31                          *

--- home-manager/j-ace-svg/fonts/default.nix
+++ home-manager/j-ace-svg/fonts/default.nix
@@ -3,9 +3,19 @@
   lib,
   ...
 }: {
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = ["DejaVu Serif"];
+      sansSerif = ["DejaVu Sans"];
+      monospace = ["DejaVu Sans Mono"];
+    };
+  };
+
   home.packages = [
     pkgs.fontforge-gtk
     (pkgs.callPackage ./ninjargon/derivation.nix {inherit pkgs;})
-    pkgs.nerd-fonts.hack
+    pkgs.nerd-fonts.dejavu-sans-mono
+    pkgs.dejavu_fonts
   ];
 }
