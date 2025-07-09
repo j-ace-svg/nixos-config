553483be8014200e7480aab1a3e7a8ceb8ad255d
John Montgomery <j.ace.svg@gmail.com>
2025-07-09 14:49:18 -0500

    wiggin: 466 current  2025-07-09 14:48:55  25.11.20250603.c2a0396  6.12.31                          *

--- home-manager/j-ace-svg/fonts/default.nix
+++ home-manager/j-ace-svg/fonts/default.nix
@@ -7,8 +7,8 @@
     enable = true;
     defaultFonts = {
       serif = ["DejaVu Serif"];
-      sansSerif = ["DejaVuSansM Nerd Font Propo" "DejaVu Sans"];
-      monospace = ["DejaVuSansM Nerd Font Mono"];
+      sansSerif = ["DejaVuSansM Nerd Font Propo" "DejaVu Sans" "Hack Nerd Font Propo"];
+      monospace = ["DejaVuSansM Nerd Font Mono" "Hack Nerd Font Mono"];
     };
   };
 
