f0d9605668a868990cd7808196f5433515e45bbe
John Montgomery <j.ace.svg@gmail.com>
2025-07-10 14:37:19 -0500

    wiggin: 483 current  2025-07-10 14:36:55  25.11.20250603.c2a0396  6.12.31                          *

--- home-manager/j-ace-svg/bash/default.nix
+++ home-manager/j-ace-svg/bash/default.nix
@@ -10,11 +10,14 @@
     enable = true;
     bindings = {
       "\\xxbeginning-of-line_" = "beginning-of-line"; # Commas improve legibility but are also now necessary
-      "\\xxkill-line_" = "kill-line"; # Commas improve legibility but are also now necessary
+      "\\xxbackward-char_" = "backward-char";
+      "\\xxdelete-char_" = "delete-char";
+      "\\xxbackward-delete-char_" = "backward-delete-char";
+      "\\xxkill-line_" = "kill-line";
       "\\xxaccept-line_" = "accept-line";
       "\\xxyank_" = "yank";
       "\\xxyank-pop_" = "yank-pop";
-      "\\C-n" = ''"\M--\xxkill-line_\xxkill-line_la\xxaccept-line_\xxyank_\xxbeginning-of-line_\xxyank_\xxyank-pop_"'';
+      "\\C-n" = ''" \M--\xxkill-line_ \xxbackward-char_\xxkill-line_la\xxaccept-line_\xxyank_\xxbeginning-of-line_\xxdelete-char_\xxyank_\xxyank-pop_\xxbackward-delete-char_"'';
     };
     variables = {
     };
