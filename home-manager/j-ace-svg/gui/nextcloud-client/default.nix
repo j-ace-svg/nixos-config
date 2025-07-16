0307aee7f3ab9b4513eab3c57c5a4b20782a1c48
John Montgomery <j.ace.svg@gmail.com>
2025-07-15 22:54:26 -0500

    wiggin: 503 current  2025-07-15 22:53:57  25.11.20250603.c2a0396  6.12.31                          *

--- home-manager/j-ace-svg/gui/nextcloud-client/default.nix
+++ home-manager/j-ace-svg/gui/nextcloud-client/default.nix
@@ -12,6 +12,60 @@ in {
       startInBackground = true;
     };
 
+    home.file.".config/Nextcloud/nextcloud.cfg" = {
+      force = true;
+      text = let
+        server = "https://nextcloud.philotic.xyz";
+        user = "j.ace.svg";
+        displayName = "J.ace.svg";
+      in ''
+        [General]
+        clientVersion=${pkgs.nextcloud-client.version}
+        confirmExternalStorage=true
+        isVfsEnabled=false
+        launchOnSystemStartup=false
+        newBigFolderSizeLimit=500
+        optionalServerNotifications=false
+        overrideLocalDir=
+        overrideServerUrl=
+        promptDeleteAllFiles=false
+        showCallNotifications=false
+        showChatNotifications=false
+        showInExplorerNavigationPane=false
+        useNewBigFolderSizeLimit=true
+
+        [Accounts]
+        0\Folders\1\ignoreHiddenFiles=false
+        0\Folders\1\journalPath=.sync_ee05714b9319.db
+        0\Folders\1\localPath=${config.home.homeDirectory}/Nextcloud/
+        0\Folders\1\paused=false
+        0\Folders\1\targetPath=/
+        0\Folders\1\version=2
+        0\Folders\1\virtualFilesMode=off
+        0\authType=webflow
+        0\dav_user=${user}
+        0\displayName=${displayName}
+        0\encryptionCertificateSha256Fingerprint=@ByteArray()
+        0\networkDownloadLimit=0
+        0\networkDownloadLimitSetting=-2
+        0\networkProxyHostName=
+        0\networkProxyNeedsAuth=false
+        0\networkProxyPort=0
+        0\networkProxySetting=0
+        0\networkProxyType=2
+        0\networkProxyUser=
+        0\networkUploadLimit=0
+        0\networkUploadLimitSetting=-2
+        0\serverColor=@Variant(\0\0\0\x43\x1\xff\xff\0\0\x82\x82\xc9\xc9\0\0)
+        0\serverHasValidSubscription=false
+        0\serverTextColor=@Variant(\0\0\0\x43\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0)
+        0\serverVersion=31.0.5.1
+        0\url=${server}
+        0\version=13
+        0\webflow_user=${user}
+      '';
+    };
+
     home.packages = [
     ];
   };
