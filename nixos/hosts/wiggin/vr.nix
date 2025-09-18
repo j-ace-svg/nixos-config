a7bb49458f1332a8034920d9b85d7835c979c072
John Montgomery <j.ace.svg@gmail.com>
2025-09-17 22:40:24 -0500

    wiggin: 555 current  2025-09-17 22:40:19  25.11.20250914.8a28cc4  6.12.47

--- nixos/hosts/wiggin/vr.nix
+++ nixos/hosts/wiggin/vr.nix
@@ -9,4 +9,28 @@
     enable = true;
     defaultRuntime = true;
   };
+
+  home-manager.users.j-ace-svg.xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+    {
+      "config" :
+      [
+        "~/.local/share/Steam/config"
+      ],
+      "external_drivers" : null,
+      "jsonid" : "vrpathreg",
+      "log" :
+      [
+        "~/.local/share/Steam/logs"
+      ],
+      "runtime" :
+      [
+        "${pkgs.opencomposite}/lib/opencomposite"
+      ],
+      "version" : 1
+    }
+  '';
+
+  environment.systemPackages = [
+    pkgs.opencomposite
+  ];
 }
