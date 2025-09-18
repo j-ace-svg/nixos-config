c5ba4c26a784ecd34166e30bfb468730380a140b
John Montgomery <j.ace.svg@gmail.com>
2025-09-18 14:00:35 -0500

    wiggin: 561 current  2025-09-18 14:00:32  25.11.20250914.8a28cc4  6.12.47

--- nixos/hosts/wiggin/vr.nix
+++ nixos/hosts/wiggin/vr.nix
@@ -10,28 +10,61 @@
     defaultRuntime = true;
   };
 
-  # Disabled - prevents steamvr from being able to write to it
-  /*
-    home-manager.users.j-ace-svg.xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    {
-      "config" :
-      [
-        "~/.local/share/Steam/config"
-      ],
-      "external_drivers" : null,
-      "jsonid" : "vrpathreg",
-      "log" :
-      [
-        "~/.local/share/Steam/logs"
-      ],
-      "runtime" :
-      [
-        "${pkgs.opencomposite}/lib/opencomposite"
-      ],
-      "version" : 1
-    }
-  '';
-  */
+  home-manager.users.j-ace-svg = {
+    xdg = {
+      desktopEntries = {
+        valve-URI-steamvr = {
+          name = "URI-steamvr";
+          comment = "URI handler for steamvr://";
+          exec = "\"/home/steamuser/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrurlhandler\" %U";
+          terminal = false;
+          noDisplay = true;
+          type = "Application";
+          categories = ["Game"];
+          mimeType = ["x-scheme-handler/steamvr"];
+        };
+        valve-URI-vrmonitor = {
+          name = "URI-vrmonitor";
+          comment = "URI handler for vrmonitor://";
+          exec = "\"/home/steamuser/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/../vrmonitor.sh\" %U";
+          terminal = false;
+          noDisplay = true;
+          type = "Application";
+          categories = ["Game"];
+          mimeType = ["x-scheme-handler/vrmonitor"];
+        };
+      };
+      mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "x-scheme-handler/steamvr" = ["valve-URI-steamvr.desktop"];
+          "x-scheme-handler/vrmonitor" = ["valve-URI-vrmonitor.desktop"];
+        };
+      };
+    };
+    # Disabled - prevents steamvr from being able to write to it
+    /*
+      xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "config" :
+        [
+          "~/.local/share/Steam/config"
+        ],
+        "external_drivers" : null,
+        "jsonid" : "vrpathreg",
+        "log" :
+        [
+          "~/.local/share/Steam/logs"
+        ],
+        "runtime" :
+        [
+          "${pkgs.opencomposite}/lib/opencomposite"
+        ],
+        "version" : 1
+      }
+    '';
+    */
+  };
 
   # Kernel patch to allow all apps to setcap. Ngl I don't understand it but
   # it seems like a bad idea; there doesn't seem like a better way tho.
