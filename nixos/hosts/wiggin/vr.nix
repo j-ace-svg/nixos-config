fb92ab75d765c6a6c823b6b758ec28b78d17e237
John Montgomery <j.ace.svg@gmail.com>
2025-09-18 02:02:11 -0500

    wiggin: 558 current  2025-09-18 02:02:04  25.11.20250914.8a28cc4  6.12.47

--- nixos/hosts/wiggin/vr.nix
+++ nixos/hosts/wiggin/vr.nix
@@ -33,6 +33,19 @@
   '';
   */
 
+  # Kernel patch to allow all apps to setcap. Ngl I don't understand it but
+  # it seems like a bad idea; there doesn't seem like a better way tho.
+  boot.kernelPatches = [
+    {
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
+    }
+  ];
+
   environment.systemPackages = [
     pkgs.opencomposite
   ];
