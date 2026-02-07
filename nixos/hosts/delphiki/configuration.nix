955727efcaab3ca44691dc21c62e556e37ded85d
John Montgomery <j.ace.svg@gmail.com>
2026-02-06 18:15:20 -0600

    delphiki: 274 current  2026-02-06 18:12:50  25.11.20251117.89c2b23  6.12.58

--- nixos/hosts/delphiki/configuration.nix
+++ nixos/hosts/delphiki/configuration.nix
@@ -11,6 +11,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./kanata/default.nix
+    ./rnnoise.nix
   ];
 
   nix = {
---
+++ nixos/hosts/delphiki/rnnoise.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services.pipewire.configPackages = [
+    (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/noise-canceling-source.conf" ''
+      context.modules = [
+      {   name = libpipewire-module-filter-chain
+          args = {
+              node.description =  "Noise Canceling Proxy"
+              media.name =  "Noise Canceling source via RNNoise"
+              filter.graph = {
+                  nodes = [
+                      {
+                          type = ladspa
+                          name = rnnoise
+                          plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so"
+                          label = noise_suppressor_stereo
+                          control = {
+                              "VAD Threshold (%)" = 50.0
+                              "VAD Grace Period (ms)" = 200
+                              "Retroactive VAD Grace (ms)" = 0
+                          }
+                      }
+                  ]
+              }
+              audio.position = [ "FL", "FR" ]
+              capture.props = {
+                  node.name =  "capture.rnnoise_source"
+                  node.passive = true
+                  audio.rate = 48000
+              }
+              playback.props = {
+                  node.name =  "rnnoise_source"
+                  media.class = Audio/Source
+                  audio.rate = 48000
+              }
+          }
+      }
+      ]
+    '')
+  ];
+}
