c41bf3ded52988583f2f4ae758cb5cf707e3d52a
John Montgomery <j.ace.svg@gmail.com>
2026-03-31 14:16:40 -0500

    wiggin: 643 current  2026-03-31 14:16:31  26.05.20260328.8110df5  6.18.20

--- nixos/hosts/wiggin/configuration.nix
+++ nixos/hosts/wiggin/configuration.nix
@@ -156,7 +156,15 @@
     kernelParams = ["intel_iommu=on"];
     blacklistedKernelModules = ["amdgpu"];
     kernelModules = ["vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio"];
-    extraModprobeConfig = "options vfio-pci ids=1002:67ef,1002:aae0";
+
+    postBootCommands = ''
+      DEVS="0000:03:00.0 0000:03:00.1"
+
+      for DEV in $DEVS; do
+        echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+      done
+      modprobe -i vfio-pci
+    '';
   };
 
   virtualisation = {
