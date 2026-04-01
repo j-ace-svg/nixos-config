4a155b282265f761d5aa69f02e64617b9e30ffd7
John Montgomery <j.ace.svg@gmail.com>
2026-04-01 00:30:35 -0500

    wiggin: 649 current  2026-04-01 00:30:32  26.05.20260328.8110df5  6.18.20

--- nixos/hosts/wiggin/vr/libvirt-qemu-hook.sh
+++ nixos/hosts/wiggin/vr/libvirt-qemu-hook.sh
@@ -1,7 +1,7 @@
 OBJECT="$1"
 OPERATION="$2"
 
-if [[ $OBJECT == "win10" ]]; then
+if [[ $OBJECT == "vr-guest-passthrough" ]]; then
         case "$OPERATION" in
                 "prepare")
                 systemctl start libvirt-nosleep@"$OBJECT"  2>&1 | tee -a /var/log/libvirt/custom_hooks.log
